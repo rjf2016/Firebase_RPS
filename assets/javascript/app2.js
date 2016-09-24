b24c1da3a6140f04e9c31c09f755d673651e6d71
Rick Fahey <rickyfahey@hotmail.com>
2016-09-24 13:33:42 -0400

    initial commit

---
+++ assets/javascript/app2.js
@@ -0,0 +1,347 @@
+//--------------------------------------------------
+// Rock,Paper Scissors multi-player using Firebase
+// 9/20/2016 RJF
+//--------------------------------------------------
+/* --- Design thought process ---
+  1) Layout multi-player HMTL5
+  2) Setup the Firebase DB
+  3) Stitch UI, Code and Firebase events together
+*/
+
+
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCXpo00q0fX-prcxijeDJaFCK3Vy-LBgKo",
+    authDomain: "rock-paper-scissors-14a81.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-14a81.firebaseio.com",
+    storageBucket: "rock-paper-scissors-14a81.appspot.com",
+    messagingSenderId: "721003169139"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var myName = null;
+var myKey = "";
+
+var playerNum = 0;  
+var playersRef = null;
+
+var p1wins = 0;
+var p2wins = 0;
+var p1losses = 0;
+var p2losses = 0;
+
+var p1Weapon = "";
+var p2Weapon = "";
+
+
+database.ref("Players/1/name").on("value", function(snapshot){
+   $(player1Name).html(snapshot.val());
+});
+
+database.ref("Players/2/name").on("value", function(snapshot){
+   $(player2Name).html(snapshot.val());
+});
+
+
+database.ref("Players/1/choice").on("value", function(snapshot){
+   var thisPlayerNum = $(myplayerNum).html();
+
+
+  if(snapshot.val()==null || thisPlayerNum != "1")
+      return;
+
+   $(myplayerChoice).html(snapshot.val());
+   
+   p1Weapon = snapshot.val();
+
+   if(thisPlayerNum == "1")  //only show if player 1
+      $(resDiv).html(p1Weapon);
+
+   database.ref("Players/turn").set("2");
+});
+
+
+database.ref("Players/2/choice").on("value", function(snapshot){
+  var thisPlayerNum = $(myplayerNum).html();
+  
+  if(snapshot.val()==null || thisPlayerNum<1)
+      return;
+  
+  p2Weapon = snapshot.val();
+  
+  if(thisPlayerNum != "2")
+      return;
+
+  $(myplayerChoice).html(snapshot.val());
+  
+  $(resDiv).html(p1Weapon + "  VS. " + p2Weapon);
+
+  if(p1Weapon == p2Weapon && p1Weapon != "")
+  {
+    $(winnerDiv).html("Tie!");
+     database.ref("Players/2/wins").set(p2wins);
+     database.ref("Players/1/losses").set(p1losses);
+  }
+  
+  
+
+  if(p1Weapon=="Rock" && p2Weapon=="Paper")
+  {
+     p1losses++;
+     p2wins++;
+     database.ref("Players/2/wins").set(p2wins);
+     database.ref("Players/1/losses").set(p1losses);
+  }
+    //$(winnerDiv).html($(player2Name).html() + " Wins");
+    
+  if(p1Weapon=="Rock" && p2Weapon=="Scissors")
+  {
+     p2losses++;
+     p1wins++;
+     database.ref("Players/1/wins").set(p1wins);
+     database.ref("Players/2/losses").set(p1losses);
+  
+   // $(winnerDiv).html($(player1Name).html() + " Wins");
+  }
+  if(p1Weapon=="Paper" && p2Weapon=="Rock")
+    {
+     p1wins++;
+     p2losses++;
+     database.ref("Players/1/wins").set(p1wins);
+     database.ref("Players/2/losses").set(p2losses);
+  }
+  
+  if(p1Weapon=="Paper" && p2Weapon=="Scissors")
+  {
+     p1losses++;
+     p2wins++;
+     database.ref("Players/2/wins").set(p2wins);
+     database.ref("Players/1/losses").set(p1losses);
+  }
+  if(p1Weapon=="Scissors" && p2Weapon=="Paper")
+    {
+     p1wins++;
+     p2losses++;
+     database.ref("Players/1/wins").set(p1wins);
+     database.ref("Players/2/losses").set(p2losses);
+    }
+
+  if(p1Weapon=="Scissors" && p2Weapon=="Rock")
+    {
+     p1losses++;
+     p2wins++;
+     database.ref("Players/2/wins").set(p2wins);
+     database.ref("Players/1/losses").set(p1losses);
+    }
+   
+   setTimeout(function(){
+        ResetGame();
+    }, 5000); 
+});
+
+database.ref("Players/1/wins").on("value", function(snapshot){
+  
+  var thisPlayerNum = $(myplayerNum).html();
+  if(snapshot.val()==0)
+      return;
+  if(thisPlayerNum != "1")
+      return;
+
+   $(winnerDiv).html($(player1Name).html() + " Wins");
+   $('#p1-wins').html(snapshot.val());
+  
+});
+
+database.ref("Players/2/wins").on("value", function(snapshot){
+  
+  var thisPlayerNum = $(myplayerNum).html();
+  var winnerName;
+  
+  if(snapshot.val()==0)
+      return;
+
+console.log("Here 1 " + snapshot.val());
+    database.ref("Players/2/name").once("value", function(snapshot){
+        winnerName = snapshot.val();
+      }
+    );
+
+  
+   $(winnerDiv).html(winnerName + " Wins");
+   console.log("Here 2");
+   $('#p2-wins').html(snapshot.val());
+  
+});
+
+database.ref("Players/1/losses").on("value", function(snapshot){
+  var thisPlayerNum = $(myplayerNum).html();
+   if(snapshot.val()==0)
+      return;
+   if(thisPlayerNum != "1")
+      return;
+ 
+   $('#p1-losses').html(snapshot.val());
+  
+});
+database.ref("Players/2/losses").on("value", function(snapshot){
+  var thisPlayerNum = $(myplayerNum).html();
+  if(snapshot.val()==null)
+      return;
+  if(thisPlayerNum != "2")
+      return;
+
+  $('#p2-losses').html(snapshot.val());
+  
+});
+
+
+;
+
+function ResetGame()
+{
+  $(resDiv).html("");
+  $(winnerDiv).html("");
+  database.ref("Players/turn").set("1");
+}
+
+
+database.ref("Players/turn/").on("value", function(snapshot){
+  
+  var thisPlayerNum = $(myplayerNum).html();
+
+  if(snapshot.val()==null || thisPlayerNum<1)
+      return;
+
+  
+   if(snapshot.val() == $(myplayerNum).html())
+      $(turnDiv).html("It's your turn");
+   else
+      $(turnDiv).html("Other players turn");
+
+ 
+  //if Turn=1 and Turn = CurrentPlayerInBrowser
+  if((snapshot.val() == 1) && $(myplayerNum).html() == 1)
+  {
+      $('#p1-weapons').css('visibility','visible');
+      $('#p2-weapons').css('visibility','hidden');
+  }
+  if((snapshot.val() == 1) && $(myplayerNum).html() == 2)
+  {
+      $('#p1-weapons').css('visibility','hidden');
+      $('#p2-weapons').css('visibility','hidden');
+  }
+  
+  if((snapshot.val() == 2) && $(myplayerNum).html() == 2)
+  { 
+      $('#p1-weapons').css('visibility','hidden');
+      $('#p2-weapons').css('visibility','visible');
+  }  
+  if((snapshot.val() == 2) && $(myplayerNum).html() == 1)
+  { 
+      $('#p1-weapons').css('visibility','hidden');
+      $('#p2-weapons').css('visibility','hidden');
+  } 
+     
+    
+});
+
+
+database.ref("Players/").on("value", function(snapshot) 
+{
+  var a = snapshot.numChildren();
+  
+   a++;
+
+   playerNum=a;
+  
+});
+
+
+database.ref("Players/").on("child_added", function(childSnapshot, prevChildKey){
+
+
+ if(childSnapshot.child("name").val() == myName)
+ {
+   $(playerInfo).html("Hi " + myName + "! You are player " + playerNum);
+   
+   $(myplayerNum).html(playerNum);
+   $(myplayerName).html(myName);
+   
+   if(playerNum==2)
+       database.ref("Players/turn").set("1");
+ }
+});
+
+
+
+function savePlayerToFB(playerName) {
+
+  myName = playerName;
+  if (playerNum==0)
+    playerNum=1;
+
+  var newPlayer = {
+      name:  playerName,
+      wins: 0,
+      losses: 0,
+      choice: ""
+    }
+ 
+ //used later
+ myKey = database.ref("Players/" + playerNum).set(newPlayer);
+
+
+};
+
+
+//Chat function
+$(document).on('click', '#sendText', function(){
+   console.log(event.target.id);
+
+   var str = $('#textMessage').val();
+   database.ref("chat").push(myName + ": " + str);
+});
+
+//Chat listener
+database.ref("chat").on("child_added", function(snapshot){
+   $('#chatText').append(snapshot.val() + '&#xA;');
+});
+
+
+$(document).on('click', '#p1-weapons', function(){
+
+    //event.target.style.fontWeight="bold";
+    database.ref("Players/" + $(myplayerNum).html() + "/choice").set(event.target.id);
+ 
+});
+
+$(document).on('click', '#p2-weapons', function(){
+  
+    console.log(event.target.id);
+
+    //event.target.style.fontWeight="bold";
+    database.ref("Players/" + $(myplayerNum).html() + "/choice").set(event.target.id);
+
+});
+
+
+$(document).on('click', '#resDiv', function(){
+   console.log("PlayerNum=" + playerNum +"; myName=" + myName);
+});
+
+
+
+ // Handle the click of the Category Submit and add the new category to the cars array ("push" to grow the array)
+$("#startGame").on("click", function()
+  {
+     playerName = $(".form-control").val();  //get the value of what the user typed in the textbox
+      
+     savePlayerToFB(playerName);
+
+  });
+
+
+ 
